bc06412490177ae6ce99a279bdeb5a598a8ce166
David C. Wallace <wallacdc@gmail.com>
2015-04-02 18:38:59 -0400

    Changed graph appearance

--- TimeHistogram.py
+++ TimeHistogram.py
@@ -1,5 +1,5 @@
 """
-Parking Fines Project, Part III - Plot a histogram
+Database Project, Part III - Plot a histogram
 David C. Wallace
 Updated March 3rd, 2015.
 """
@@ -16,10 +16,18 @@ with open('timehist.txt', 'rU') as hist:
 
 histogram = np.array(data, dtype=float)
 transposed = np.transpose(histogram)
+avx = []
+avy = []
+for i in range(1, len(transposed[1])-1):
+    avx.append(transposed[0][i])
+    avy.append((transposed[1][i-1]+transposed[1][i]+transposed[1][i+1])/3)
 
-plt.plot(transposed[0], transposed[1], 'o-')
+plt.figure()
+plt.plot(avx, avy, color='gray', linewidth='4.0')
+plt.plot(transposed[0], transposed[1], 'o', color='red')
 plt.axis([0, 24, 0, 100000])
-plt.ylabel("Number of Fines Issued (2 Year Total)")
-plt.xlabel("Time of Day (24h)")
+plt.text(13.5, 80000, '3hr Moving Average', color='gray', style='oblique')
+plt.ylabel("Number of Fines Issued", style='italic')
+plt.xlabel("Time of Day (24h)", style='italic')
 plt.title("Baltimore City Parking Violations, Feb 2013 - Feb 2015")
 plt.show()
